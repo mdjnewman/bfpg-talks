7d0c6441a93e6735b193b20ff0ea12b8c5594af0
Matt Newman <mdjnewman@gmail.com>
2016-05-19 23:20:26 +1000

    Clean up deploy.sh & travis.yml a little

--- deploy.sh
+++ deploy.sh
@@ -36,8 +36,6 @@ doCompile
 
 # Now let's go have some fun with the cloned repo
 cd _site
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet ; then
@@ -48,7 +46,7 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add --all .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+git commit --author 'Travis CI <>' -m "Deploy to GitHub Pages: ${SHA}"
 
 openssl aes-256-cbc -K $encrypted_bf200f1b2bff_key -iv $encrypted_bf200f1b2bff_iv -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
